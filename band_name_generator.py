11c5d0eb9907924f4b4f76ffd0cb6801873dca21
zainmehdicoding <zainshah1911@gmail.com>
2025-08-30 16:00:31 +0500

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+City = input("What is the name of the city you grew up in?\n")
+Pet = input("What is the name of your pet?\n")
+print("Your band name could be " + City + " " + Pet)
+input("Press Enter to exit...")
+# name = "Zain"
+# print(name)
